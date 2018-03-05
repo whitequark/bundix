87c2f9181df888004f716754190550de906b8d27
Judson Lester <nyarly@users.noreply.github.com>
2018-03-05 07:54:54 -0800

    Groups and Platforms added from gemfile to gemset.nix (#27)
    * Test for source conversion
    
    * Adding Nixer to serialize Ruby objects to Nix expressions
    
    * Removing object2nix class method
    
    * Respecting --gemfile switch
    
    * Trivial: retaining the order of flag definitions
    
    * Tidying up, test for context set up correctly
    
    * Using Nixer to serialize values
    
    * Groups and Platforms added from gemfile to gemset.nix

--- lib/bundix.rb
+++ lib/bundix.rb
@@ -28,6 +28,15 @@ class Bundix
 
   attr_accessor :fetcher
 
+  class Dependency < Bundler::Dependency
+    def initialize(name, version, options={}, &blk)
+      super(name, version, options, &blk)
+      @bundix_version = version
+    end
+
+    attr_reader :version
+  end
+
   def initialize(options)
     @options = { quiet: false, tempfile: nil }.merge(options)
     @fetcher = Fetcher.new
@@ -36,10 +45,11 @@ class Bundix
   def convert
     cache = parse_gemset
     lock = parse_lockfile
+    dep_cache = build_depcache(lock)
 
     # reverse so git comes last
     lock.specs.reverse_each.with_object({}) do |spec, gems|
-      gem = find_cached_spec(spec, cache) || convert_spec(spec, cache)
+      gem = find_cached_spec(spec, cache) || convert_spec(spec, cache, dep_cache)
       gems.merge!(gem)
 
       if spec.dependencies.any?
@@ -48,8 +58,46 @@ class Bundix
     end
   end
 
-  def convert_spec(spec, cache)
-    {spec.name => {version: spec.version.to_s, source: Source.new(spec, fetcher).convert}}
+  def groups(spec, dep_cache)
+    {groups: dep_cache.fetch(spec.name).groups}
+  end
+
+  PLATFORM_MAPPING = {}
+
+  {
+    "ruby" => [{engine: "ruby"}, {engine:"rbx"}, {engine:"maglev"}],
+    "mri" => [{engine: "ruby"}, {engine: "maglev"}],
+    "rbx" => [{engine: "rbx"}],
+    "jruby" => [{engine: "jruby"}],
+    "mswin" => [{engine: "mswin"}],
+    "mswin64" => [{engine: "mswin64"}],
+    "mingw" => [{engine: "mingw"}],
+    "x64_mingw" => [{engine: "mingw"}],
+  }.each do |name, list|
+    PLATFORM_MAPPING[name] = list
+    %w(1.8 1.9 2.0 2.1 2.2 2.3 2.4 2.5).each do |version|
+      PLATFORM_MAPPING["#{name}_#{version.sub(/[.]/,'')}"] = list.map do |platform|
+        platform.merge(:version => version)
+      end
+    end
+  end
+
+  def platforms(spec, dep_cache)
+    # c.f. Bundler::CurrentRuby
+    platforms = dep_cache.fetch(spec.name).platforms.map do |platform_name|
+      PLATFORM_MAPPING[platform_name.to_s]
+    end.flatten
+
+    {platforms: platforms}
+  end
+
+  def convert_spec(spec, cache, dep_cache)
+    {
+      spec.name => {
+        version: spec.version.to_s,
+        source: Source.new(spec, fetcher).convert
+      }.merge(platforms(spec, dep_cache)).merge(groups(spec, dep_cache))
+    }
   rescue => ex
     warn "Skipping #{spec.name}: #{ex}"
     puts ex.backtrace
@@ -76,6 +124,46 @@ class Bundix
     {name => cached} if cached
   end
 
+  def build_depcache(lock)
+    definition = Bundler::Definition.build(options[:gemfile], options[:lockfile], false)
+    dep_cache = {}
+
+    definition.dependencies.each do |dep|
+      dep_cache[dep.name] = dep
+    end
+
+    lock.specs.each do |spec|
+      dep_cache[spec.name] ||= Dependency.new(spec.name, nil, {})
+    end
+
+    begin
+      changed = false
+      lock.specs.each do |spec|
+        as_dep = dep_cache.fetch(spec.name)
+
+        spec.dependencies.each do |dep|
+          cached = dep_cache.fetch(dep.name) do |name|
+            if name != "bundler"
+              raise KeyError, "Gem dependency '#{name}' not specified in #{lockfile}"
+            end
+            dep_cache[name] = Dependency.new(name, lock.bundler_version, {})
+          end
+
+          if !((as_dep.groups - cached.groups) - [:default]).empty? or !(as_dep.platforms - cached.platforms).empty?
+            changed = true
+            dep_cache[cached.name] = (Dependency.new(cached.name, nil, {
+              "group" => as_dep.groups | cached.groups,
+              "platforms" => as_dep.platforms | cached.platforms
+            }))
+
+            cc = dep_cache[cached.name]
+          end
+        end
+      end
+    end while changed
+
+    return dep_cache
+  end
 
   def parse_gemset
     path = File.expand_path(options[:gemset])
