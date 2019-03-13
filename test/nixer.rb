db11153798a106f112e15717918924ee87427bf8
Burke Libbey <burke@libbey.me>
2019-03-13 15:01:31 -0400

    Fix existing test, unrelated to this PR

--- test/nixer.rb
+++ test/nixer.rb
@@ -16,7 +16,7 @@ class TestNixer < Minitest::Test
 
   def test_object2nix_array_of_hash
     assert_equal(
-      "[{\n  a = \"x\";\n  b = \"7\";\n} {\n a = \"y\";\n c = \"8\";\n}]",
+      "[{\n  a = \"x\";\n  b = \"7\";\n} {\n  a = \"y\";\n  c = \"8\";\n}]",
       Bundix::Nixer.new([{:a => "x", :b => "7"}, {:a => "y", :c => "8"}]).serialize
     )
   end
